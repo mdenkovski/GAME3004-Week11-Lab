141c87d81b18145048249589ea8bcc4991777357
Michael Denkovski <michaeldenkovski@hotmail.com>
2021-03-25 16:46:19 -0400

    setting up alien soldier enemy prefab

--- Assets/_Scripts/EnemyBehaviour.cs
+++ Assets/_Scripts/EnemyBehaviour.cs
@@ -3,21 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+[System.Serializable]
+public enum ZombieState
+{
+    IDLE,
+    RUN,
+    JUMP,
+    PUNCH,
+    DIE
+}
+
+
+[RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(Animator))]
 public class EnemyBehaviour : MonoBehaviour
 {
     public NavMeshAgent navMeshAgent;
     public PlayerBehaviour player;
+    public Animator controller;
+
 
     // Start is called before the first frame update
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
         player = FindObjectOfType<PlayerBehaviour>();
+        controller = GetComponent<Animator>();
+
+        controller.SetInteger("AnimState", (int)ZombieState.RUN);
     }
 
     // Update is called once per frame
     void Update()
     {
-        navMeshAgent.SetDestination(player.transform.position);
+        if (player)
+        {
+            navMeshAgent.SetDestination(player.transform.position);
+
+        }
     }
 }
